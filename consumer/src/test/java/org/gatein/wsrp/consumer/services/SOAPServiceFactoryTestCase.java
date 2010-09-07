1d8dde7dcfd912c7facd07b2cbaf5d1e050a0a43
Chris Laprun <metacosm@gmail.com>
2010-09-07 11:20:54 +0000

    - GTNWSRP-68: added test case.

--- consumer/src/test/java/org/gatein/wsrp/consumer/services/SOAPServiceFactoryTestCase.java
+++ consumer/src/test/java/org/gatein/wsrp/consumer/services/SOAPServiceFactoryTestCase.java
@@ -52,6 +52,12 @@ public class SOAPServiceFactoryTestCase extends TestCase
       factory = new SOAPServiceFactory();
    }
 
+   public void testV2ServiceWithExtraPorts() throws Exception
+   {
+      factory.setWsdlDefinitionURL("http://stacks3.mw.lab.eng.bos.redhat.com:7001/wsrp/producer/wsrp-2.0/markup?WSDL");
+      checkPorts(WSRP2_PORT_TYPES);
+   }
+
    public void testSimpleV2Service() throws Exception
    {
       factory.setWsdlDefinitionURL("http://www.netunitysoftware.com/wsrp2interop/WsrpProducer.asmx?Operation=WSDL&WsrpVersion=Two");
