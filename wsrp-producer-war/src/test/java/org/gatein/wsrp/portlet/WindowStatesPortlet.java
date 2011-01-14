97d7394e21ef0849c35670a2da29710dc8405ce5
mvanco@redhat.com <mvanco@redhat.com>
2011-01-14 16:11:17 +0000

    WSRP2 Sample portlets  - added new sample portlet for window states

---
+++ wsrp-producer-war/src/test/java/org/gatein/wsrp/portlet/WindowStatesPortlet.java
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * JBoss, a division of Red Hat                                               *
+ * Copyright 2010, Red Hat Middleware, LLC, and individual                    *
+ * contributors as indicated by the @authors tag. See the                     *
+ * copyright.txt in the distribution for a full listing of                    *
+ * individual contributors.                                                   *
+ *                                                                            *
+ * This is free software; you can redistribute it and/or modify it            *
+ * under the terms of the GNU Lesser General Public License as                *
+ * published by the Free Software Foundation; either version 2.1 of           *
+ * the License, or (at your option) any later version.                        *
+ *                                                                            *
+ * This software is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU           *
+ * Lesser General Public License for more details.                            *
+ *                                                                            *
+ * You should have received a copy of the GNU Lesser General Public           *
+ * License along with this software; if not, write to the Free                *
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA         *
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.                   *
+ ******************************************************************************/
+package org.gatein.wsrp.portlet;
+
+import java.io.IOException;
+
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.WindowState;
+
+/**
+ * @author <a href="mailto:mvanco@redhat.com">Michal Vanco</a>
+ * @version $Revision$
+ */
+public class WindowStatesPortlet extends GenericPortlet
+{
+
+   @Override
+   public void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException
+   {
+      response.setContentType("text/html");
+
+      StringBuffer sb = new StringBuffer();
+      sb.append("<h1>Welcome to window states sample</h1>");
+      sb.append("now you have <b>${state}</b> state selected.");
+      WindowState state = request.getWindowState();
+      response.getWriter().print(sb.toString().replace("${state}", state.toString()));
+   }
+
+}
