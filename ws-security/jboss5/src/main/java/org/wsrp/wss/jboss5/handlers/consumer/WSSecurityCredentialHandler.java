5eece3b24859a2486ad64125b89d82d9180bda93
mposolda <mposolda@gmail.com>
2011-11-23 22:25:44 +0000

    Reverting previous commit

--- ws-security/jboss5/src/main/java/org/wsrp/wss/jboss5/handlers/consumer/WSSecurityCredentialHandler.java
+++ ws-security/jboss5/src/main/java/org/wsrp/wss/jboss5/handlers/consumer/WSSecurityCredentialHandler.java
@@ -36,7 +36,6 @@ import org.gatein.wsrp.servlet.ServletAccess;
 import org.jboss.ws.core.CommonMessageContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.wsrp.wss.credentials.CredentialsAccessor;
 
 /**
  * @author <a href="mailto:mwringe@redhat.com">Matt Wringe</a>
@@ -45,13 +44,6 @@ import org.wsrp.wss.credentials.CredentialsAccessor;
 public class WSSecurityCredentialHandler implements SOAPHandler<SOAPMessageContext>
 {
    private static Logger log = LoggerFactory.getLogger(WSSecurityCredentialHandler.class);
-
-   private CredentialsAccessor credentialsAccessor;
-
-   public WSSecurityCredentialHandler(CredentialsAccessor credentialsAccessor)
-   {
-      this.credentialsAccessor = credentialsAccessor;
-   }
 
    public void close(MessageContext arg0)
    {
@@ -84,16 +76,24 @@ public class WSSecurityCredentialHandler implements SOAPHandler<SOAPMessageConte
 
          CommonMessageContext ctx = (CommonMessageContext) soapMessageContext;
 
-         Credentials credentials  = credentialsAccessor.getCredentials();
-
-         if (credentials != null)
+         HttpServletRequest request = ServletAccess.getRequest();
+         if (request != null && request.getSession() != null)
          {
-            ctx.put(BindingProvider.USERNAME_PROPERTY, credentials.getUsername());
-            ctx.put(BindingProvider.PASSWORD_PROPERTY, credentials.getPassword());
+             Credentials credentials  = (Credentials)request.getSession().getAttribute(Credentials.CREDENTIALS);
+             if (credentials != null)
+             {
+                 ctx.put(BindingProvider.USERNAME_PROPERTY, credentials.getUsername());
+                 ctx.put(BindingProvider.PASSWORD_PROPERTY, credentials.getPassword());
+             }
+             else
+             {
+                 log.debug("Could not find credentials to put in WS-Security header");
+                 return true;
+             }
          }
          else
          {
-            log.debug("Could not find credentials to put in WS-Security header");
+             log.debug("Could not get current HttpServletRequest, cannot login.");
          }
       }
       catch (Exception e)
