0aad2f9387e260f25512ada49ecd4b7342057146
Chris Laprun <metacosm@gmail.com>
2012-02-03 09:35:22 +0000

    - GTNWSRP-268: Applied patch from mposolda.

--- ws-security/jboss5/src/main/java/org/wsrp/wss/jboss5/handlers/consumer/WSSecurityCredentialHandler.java
+++ ws-security/jboss5/src/main/java/org/wsrp/wss/jboss5/handlers/consumer/WSSecurityCredentialHandler.java
@@ -1,41 +1,39 @@
-/******************************************************************************
- * JBoss, a division of Red Hat                                               *
- * Copyright 2011, Red Hat Middleware, LLC, and individual                    *
- * contributors as indicated by the @authors tag. See the                     *
- * copyright.txt in the distribution for a full listing of                    *
- * individual contributors.                                                   *
- *                                                                            *
- * This is free software; you can redistribute it and/or modify it            *
- * under the terms of the GNU Lesser General Public License as                *
- * published by the Free Software Foundation; either version 2.1 of           *
- * the License, or (at your option) any later version.                        *
- *                                                                            *
- * This software is distributed in the hope that it will be useful,           *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU           *
- * Lesser General Public License for more details.                            *
- *                                                                            *
- * You should have received a copy of the GNU Lesser General Public           *
- * License along with this software; if not, write to the Free                *
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA         *
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.                   *
- ******************************************************************************/
+/*
+ * JBoss, a division of Red Hat
+ * Copyright 2012, Red Hat Middleware, LLC, and individual
+ * contributors as indicated by the @authors tag. See the
+ * copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.wsrp.wss.jboss5.handlers.consumer;
 
-import java.util.Set;
+import org.gatein.wci.security.Credentials;
+import org.gatein.wsrp.wss.credentials.CredentialsAccessor;
+import org.jboss.ws.core.CommonMessageContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.xml.namespace.QName;
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
-
-import org.gatein.wci.security.Credentials;
-import org.gatein.wsrp.servlet.ServletAccess;
-import org.jboss.ws.core.CommonMessageContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Set;
 
 /**
  * @author <a href="mailto:mwringe@redhat.com">Matt Wringe</a>
@@ -44,6 +42,13 @@ import org.slf4j.LoggerFactory;
 public class WSSecurityCredentialHandler implements SOAPHandler<SOAPMessageContext>
 {
    private static Logger log = LoggerFactory.getLogger(WSSecurityCredentialHandler.class);
+
+   private CredentialsAccessor credentialsAccessor;
+
+   public WSSecurityCredentialHandler(CredentialsAccessor credentialsAccessor)
+   {
+      this.credentialsAccessor = credentialsAccessor;
+   }
 
    public void close(MessageContext arg0)
    {
@@ -74,26 +79,18 @@ public class WSSecurityCredentialHandler implements SOAPHandler<SOAPMessageConte
 
          log.debug("Attempting to convert security context to WS-Security header");
 
-         CommonMessageContext ctx = (CommonMessageContext) soapMessageContext;
-         
-         HttpServletRequest request = ServletAccess.getRequest();
-         if (request != null && request.getSession() != null)
+         CommonMessageContext ctx = (CommonMessageContext)soapMessageContext;
+
+         Credentials credentials = credentialsAccessor.getCredentials();
+
+         if (credentials != null)
          {
-             Credentials credentials  = (Credentials)request.getSession().getAttribute(Credentials.CREDENTIALS);
-             if (credentials != null)
-             {
-                 ctx.put(BindingProvider.USERNAME_PROPERTY, credentials.getUsername());
-                 ctx.put(BindingProvider.PASSWORD_PROPERTY, credentials.getPassword());
-             }
-             else
-             {
-                 log.debug("Could not find credentials to put in WS-Security header");
-                 return true;
-             }
+            ctx.put(BindingProvider.USERNAME_PROPERTY, credentials.getUsername());
+            ctx.put(BindingProvider.PASSWORD_PROPERTY, credentials.getPassword());
          }
          else
          {
-             log.debug("Could not get current HttpServletRequest, cannot login.");
+            log.debug("Could not find credentials to put in WS-Security header");
          }
       }
       catch (Exception e)
