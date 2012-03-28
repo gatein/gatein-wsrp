e601cbb55f1ebc16215f050b5a60d15ae7ae24ca
Chris Laprun <metacosm@gmail.com>
2012-03-28 11:26:45 +0000

    Bug 807626 / GTNWSRP-283 - Producer registration error when producer is set to not use registrations: registration used to track consumers when registration is not required on the producer wasn't added to the consumer that was just created resulting in improper state.

--- producer/src/main/java/org/gatein/registration/impl/RegistrationManagerImpl.java
+++ producer/src/main/java/org/gatein/registration/impl/RegistrationManagerImpl.java
@@ -283,10 +283,10 @@ public class RegistrationManagerImpl implements RegistrationManager
       Consumer unregConsumer = getConsumerByIdentity(NON_REGISTERED_CONSUMER);
       if (unregConsumer == null)
       {
-         unregConsumer = createConsumer(NON_REGISTERED_CONSUMER);
-         Registration registration = addRegistrationTo(NON_REGISTERED_CONSUMER, new HashMap<QName, Object>(), null, false);
+         Registration registration = addRegistrationTo(NON_REGISTERED_CONSUMER, new HashMap<QName, Object>(), null, true);
          registration.setStatus(RegistrationStatus.VALID);
-         getPersistenceManager().saveChangesTo(unregConsumer);
+         getPersistenceManager().saveChangesTo(registration);
+         return registration;
       }
       //The unregistered consumer should only ever have one registration, return that
       return unregConsumer.getRegistrations().iterator().next();
