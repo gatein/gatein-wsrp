6a4bb534690d2939a08bb3220a53fc628ec81249
Matt Wringe <mwringe@gmail.com>
2012-06-01 16:48:19 -0400

    GTNWSRP-288: Modified the ws-security configuration to use the JBossWS native post handlers instead of the endpoint handlers. This allows for encryption to function properly as when the endpoint is accessed it will already be in an accessible, decrypted state.

--- ws-security/wss/src/main/java/org/gatein/wsrp/wss/CustomizePortListener.java
+++ ws-security/wss/src/main/java/org/gatein/wsrp/wss/CustomizePortListener.java
@@ -1,6 +1,6 @@
 /******************************************************************************
  * JBoss, a division of Red Hat                                               *
- * Copyright 2011, Red Hat Middleware, LLC, and individual                    *
+ * Copyright 2012, Red Hat Middleware, LLC, and individual                    *
  * contributors as indicated by the @authors tag. See the                     *
  * copyright.txt in the distribution for a full listing of                    *
  * individual contributors.                                                   *
@@ -20,33 +20,14 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA         *
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.                   *
  ******************************************************************************/
-package org.wsrp.wss.jboss5.handlers.producer;
+package org.gatein.wsrp.wss;
 
-import javax.xml.ws.handler.MessageContext;
-
-import org.jboss.ws.extensions.security.jaxws.WSSecurityHandler;
 /**
  * @author <a href="mailto:mwringe@redhat.com">Matt Wringe</a>
  * @version $Revision$
  */
-public class JBWSSecurityHandlerWrapper extends WSSecurityHandler
+public interface CustomizePortListener
 {
-   
-   protected boolean handleInbound(MessageContext msgContext)
-   {
-      return handleInboundSecurity(msgContext);
-   }
-
-   protected boolean handleOutbound(MessageContext msgContext)
-   {
-      return handleOutboundSecurity(msgContext);
-   }
-
-   @Override
-   protected String getConfigResourceName()
-   {
-      return "conf/gatein-wsse-producer.xml";
-   }
-
+   public void customizePort(Object service);
 }
 
